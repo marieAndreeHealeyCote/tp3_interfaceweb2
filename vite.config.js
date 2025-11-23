6b834aa5de0e998fafe33cea77ce592645d00ea4
Marie-Andrée Healey-Côté <amy146892@hotmail.com>
2025-11-23 17:11:56 -0500

    ajouter plugins vite

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,32 @@
 import { defineConfig } from "vite";
 import tailwindcss from "@tailwindcss/vite";
 
+import viteImageMin from "vite-plugin-imagemin";
+import { viteStaticCopy } from "vite-plugin-static-copy";
+
 export default defineConfig({
-    plugins: [tailwindcss()],
+    plugins: [
+        tailwindcss(),
+        viteStaticCopy({
+            targets: [
+                {
+                    src: "assets/img/*",
+                    dest: "assets/img",
+                }
+            ],
+        }),
+        viteImageMin({
+            mozjpeg: {
+                quality: 50,
+            },
+            optipng: {
+                optimizationLevel: 5,
+            },
+            webp: {
+                quality: 50,
+            },
+        }
+
+        ),
+    ],
 });
